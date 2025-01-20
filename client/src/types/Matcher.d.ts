e03950fecac2c9d0420249c344cd06b741b6bc62
Robyn F H Veitch <oddert@gmail.com>
2025-01-20 11:12:17 +0000

    refactor: removed client to a seperate repository (the original repo on github)

--- client/src/types/Matcher.d.ts
+++
@@ -1,8 +0,0 @@
-export declare interface Matcher {
-    id: number,
-    match: string,
-    match_type: 'exact'|'start'|'end'|'any',
-    case_sensitive: boolean|1|0,
-    created_on: string,
-    updated_on: string,
-}
--- client/src/types/Request.d.ts
+++
@@ -1,6 +0,0 @@
-export interface ResponseData<Payload> {
-    status: number
-    message?: string
-    error?: string
-    payload?: Payload
-}
