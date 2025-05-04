66ae24814db78adf9a9e144451d573b05c741c7f
Robyn F H Veitch <oddert@gmail.com>
2025-05-04 23:21:51 +0100

    change: added new config for porstgress databases on staging and production

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,8 +1,51 @@
+// const knex = require( 'knex' );
+const dayjs = require('dayjs');
+const { types } = require('pg');
+const { builtins } = require ('pg-types');
+
+const parseFn = (val) => {
+  return val === null ? null : dayjs(val).format('YYYY-MM-DD HH:mm:ss');
+};
+
+types.setTypeParser(builtins.TIMESTAMPTZ, parseFn);
+types.setTypeParser(builtins.TIMESTAMP, parseFn);
+
+// knex.on('query', (queryData) => {
+//     console.log(queryData);
+// });
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 const config = {
-
+    // development: {
+    //     client: 'pg',
+    //     connection: {
+    //         user: 'postgres',
+    //         password: 'examplepassword',
+    //         database: 'mydatabase',
+    //         host: 'host.docker.internal',
+    //         port: 5432,
+    //         // host: '127.0.0.1',
+    //         // charset: 'utf8mb4',
+    //         // timezone: 'bst',
+    //         // typeCast: (field, next) => {
+    //         //     console.log(field.type)
+    //         //     if (field.type === 'DATE') {
+    //         //         console.log(field)
+    //         //         return field.string();
+    //         //     }
+    //         //     return next();
+    //         // },
+    //     },
+    //     useNullAsDefault: true,
+    //     migrations: {
+    //         directory: __dirname + '/db/migrations'
+    //     },
+    //     seeds: {
+    //         directory: __dirname + '/db/seeds'
+    //     }
+    // },
     development: {
         client: 'sqlite3',
         connection: {
@@ -13,14 +56,17 @@ const config = {
             directory: __dirname + '/db/migrations'
         },
         seeds: {
-            directory: __dirname + '/db/seeds'
+            directory: __dirname + '/db/private-seeds'
         }
     },
-
     staging: {
-        client: 'sqlite3',
+        client: 'pg',
         connection: {
-            filename: __dirname + '/db/personal-finance.staging.db3'
+            user: 'postgres',
+            password: 'mysecretpassword',
+            database: 'mydatabase',
+            host: 'host.docker.internal',
+            port: 5433,
         },
         useNullAsDefault: true,
         migrations: {
@@ -30,23 +76,27 @@ const config = {
             directory: __dirname + '/db/seeds'
         }
     },
-
     production: {
-        client: 'postgresql',
+        client: 'pg',
         connection: {
-            database: 'my_db',
-            user:     'username',
-            password: 'password'
+            user: 'postgres',
+            password: 'mysecretpassword',
+            database: 'mydatabase',
+            host: 'host.docker.internal',
+            port: 5432,
         },
         pool: {
             min: 2,
             max: 10
         },
+        useNullAsDefault: true,
         migrations: {
-            tableName: 'knex_migrations'
+            directory: __dirname + '/db/migrations'
+        },
+        seeds: {
+            directory: __dirname + '/db/seeds'
         }
-    }
-
+    },
 }
 
 module.exports = config
